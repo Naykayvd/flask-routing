8edda08372cc15d68889e9ecc51c190eec24d261
NayKayvd <vandiemennahum@gmail.com>
2021-07-13 16:15:06 +0200

    added

--- routing.py
+++ routing.py
@@ -1,8 +1,10 @@
+# imports
 from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 
+# user redirect
 @app.route('/user/<name>')
 def user_page(name):
     if name == "nahum":
@@ -11,11 +13,13 @@ def user_page(name):
         return redirect(url_for('guest_page', name=name))
 
 
+# admin page
 @app.route('/admin/<name>')
 def admin_page(name):
     return "I am the admin. My name is %s" % name
 
 
+# guest page
 @app.route('/guest/<name>')
 def guest_page(name):
     return "I am on the guest page. My name is %s" % name
